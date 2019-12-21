f4850e984a35d2ba87fdf95c188e22a6da8a0e8c
Jonathan Koss <jonathan_koss@yahoo.com>
2019-12-21 23:42:39 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  turn_num = 1 
+  turn_num = 0 
 
-  while turn_num <= 9 
+  while turn_num == 9 
     turn(board)
     turn_num += 1 
   end 
